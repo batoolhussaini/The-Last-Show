929a270dedaa7c54333ed9137d27ac7be6e82d1e
Batool <batool.hussainisyeda@ucalgary.ca>
2023-04-19 22:53:30 -0600

    simple layout stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
+
 function App() {
-  return <h1>The Last Show</h1>;
+  const [collapse, setCollapse] = useState(false);
+  return (
+    <div id="container">
+      <header>
+        <aside>
+          <button id="menu-button" onClick={() => setCollapse(!collapse)}>
+            + New Obituary
+          </button>
+        </aside>
+        <div id="app-header">
+          <h1>
+            The Last Show
+          </h1>
+        </div>
+        <aside>&nbsp;</aside>
+      </header>
+    </div>
+  );
+
 }
 
 export default App;
---
+++ src/Empty.js
@@ -0,0 +1,6 @@
+function Empty() {
+    return <div id="empty-holder">No Obituary yet.</div>;
+  }
+  
+  export default Empty;
+  
