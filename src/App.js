9ad92c284f69dddf5be5073db8386b187f055961
teresa <teresa.lavoie@ucalgary.ca>
2023-04-20 18:19:09 -0600

    done obituary form: without generating function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import { useState } from "react";
-
+import ObituaryForm from "./ObituaryForm";
 
 function App() {
-  const [collapse, setCollapse] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div id="container">
       <header>
         <aside>
-          <button id="menu-button" onClick={() => setCollapse(!collapse)}>
+          <button id="menu-button" onClick={handleOpenModal}>
             + New Obituary
           </button>
         </aside>
         <div id="app-header">
-          <h1>
-            The Last Show
-          </h1>
+          <h1>The Last Show</h1>
         </div>
         <aside>&nbsp;</aside>
       </header>
+
+      {showModal && (
+        <div id="modal">
+          <div id="modal-content">
+            {/* <button id="close-button" onClick={handleCloseModal}>
+              X
+            </button> */}
+            <ObituaryForm />
+          </div>
+        </div>
+      )}
     </div>
   );
-
 }
 
 export default App;
+
---
+++ src/ObituaryForm.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import App from "./App";
+
+function ObituaryForm() {
+  const [name, setName] = useState("");
+  const [picture, setPicture] = useState(null);
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [dateOfDeath, setDateOfDeath] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handlePictureChange = (event) => {
+    setPicture(event.target.files[0]);
+  };
+
+  const handleDateOfBirthChange = (event) => {
+    setDateOfBirth(event.target.value);
+  };
+
+  const handleDateOfDeathChange = (event) => {
+    setDateOfDeath(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!name || !picture || !dateOfBirth || !dateOfDeath) {
+      alert("Please fill out all fields.");
+      return;
+    }
+    setIsSubmitting(true);
+    // Call API to generate obituary
+    try {
+      const response = await fetch("your-api-endpoint", {
+        method: "POST",
+        body: new FormData(event.target),
+      });
+      // Handle response
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+  <div className="form-container">
+  <form className="obituary-form">
+    <button id="close-button" onClick={App.handleCloseModal}>
+      X
+    </button>
+    <h2>Create a New Obituary</h2>
+
+    <div className="flowers">
+        <img src="https://www.filepicker.io/api/file/CgtL9WUYQsasouSqXWtx" alt="Flowers" width="200" height="50"/>
+    </div>
+
+    <div>
+      <label htmlFor="name">Name: </label> 
+      <input type="text" id="name" value={name} onChange={handleNameChange} />
+    </div>
+
+    <div>
+      <label htmlFor="picture">Picture: </label>
+      <input type="file" id="picture" onChange={handlePictureChange} />
+    </div>
+
+    <div>
+    <label htmlFor="dateOfBirth">Date of Birth: </label>
+      <input
+        type="datetime-local"
+        id="dateOfBirth"
+        value={dateOfBirth}
+        onChange={handleDateOfBirthChange}
+      />
+    </div>
+
+    <div>
+    <label htmlFor="dateOfDeath">Date of Death: </label>
+      <input
+        type="datetime-local"
+        id="dateOfDeath"
+        value={dateOfDeath}
+        onChange={handleDateOfDeathChange}
+      />
+    </div>
+
+    <div className="form-input">
+      <button id="submit-button" type="submit" disabled={isSubmitting} onClick={handleSubmit}>
+        {isSubmitting ? "Generating Obituary..." : "Generate Obituary"}
+      </button>
+    </div>
+
+  </form>
+  </div>
+  );
+}
+
+export default ObituaryForm;
